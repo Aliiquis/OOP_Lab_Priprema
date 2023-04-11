062ba34f356092bb6f0790d162f984440e7d9f22
Aliiquis <fran.blazevic0@gmail.com>
2023-04-11 12:33:13 +0200

    Added OOP-Demo-2

--- src/main/java/hr/fer/oop/demo2/blockchain/Blockchain.java
+++ src/main/java/hr/fer/oop/demo2/blockchain/Blockchain.java
@@ -16,27 +16,21 @@ public class Blockchain {
     public int add(Block newBlock) {
         if (size == maxBlocks) return -1;
         if (blocks[0] == null) {
-            newBlock.setPrevHash(newBlock.hash(new byte[]{0}));
+            newBlock.setPrevHash(new byte[]{0});
         } else {
             Block prevBlock = blocks[size - 1];
-            newBlock.setPrevHash(newBlock.hash(prevBlock.getPrevHash()));
+            newBlock.setPrevHash(prevBlock.hash(prevBlock.getPrevHash()));
         }
         blocks[size] = newBlock;
         return size++;
     }
 
     public boolean isAltered(int blockIndex, byte[] expectedHash) {
-        for (int i = 0; i <= blockIndex; i++) {
-            if (i == 0) {
-                if (!Arrays.equals(blocks[0].getPrevHash(), blocks[0].hash(new byte[]{0}))) {
-                    return true;
-                }
-            } else {
-                if (!Arrays.equals(blocks[i].getPrevHash(), blocks[i].hash(blocks[i - 1].getPrevHash()))) {
-                    return true;
-                }
+        for (int i = 1; i <= blockIndex; i++) {
+            if (!Arrays.equals(blocks[i].getPrevHash(), blocks[i - 1].hash(blocks[i - 1].getPrevHash()))) {
+                return true;
             }
         }
-        return !Arrays.equals(blocks[blockIndex].getPrevHash(), expectedHash);
+        return !Arrays.equals(blocks[blockIndex].hash(blocks[blockIndex].getPrevHash()), expectedHash);
     }
 }
